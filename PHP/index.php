426e5278414278394f16c0eba36bee2c4b30d1f1
Dominique R. Libardo <72533293+d-libards@users.noreply.github.com>
2023-08-27 17:26:42 +0800

    function

--- PHP/index.php
+++ PHP/index.php
@@ -1,32 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="index.php" method="post">
-        <input type="checkbox" name="foods[]" value="Pizza">
-        Pizza <br>
-        <input type="checkbox" name="foods[]" value="Hamburger">
-        Hamburger <br>
-        <input type="checkbox" name="foods[]" value="Taco">
-        Taco <br>
-        <input type="checkbox" name="foods[]" value="Hotdog">
-        Hotdog <br>
-        <input type="submit" name="submit">
-    </form>
-</body>
-</html>
-
 <?php
-    if(isset($_POST['submit'])){
-        
-        $foods = $_POST['foods'];
 
-        foreach($foods as $food){
-            echo "{$food} <br>";
-        }
+    function hypotenuse( $a, $b){
+        $c = sqrt($a ** 2 + $b ** 2);
+        return $c;
     }
+
+    echo hypotenuse(4, 4);
+
 ?>
