9f23a5a9365b8b1d800c60c85ed4fba91990dc9b
Dominique R. Libardo <72533293+d-libards@users.noreply.github.com>
2023-08-27 15:18:33 +0800

    isset & empty

--- PHP/index.php
+++ PHP/index.php
@@ -7,23 +7,37 @@
 </head>
 <body>
     <form action="index.php" method="post">
-        <label for="">Enter a country</label>
-        <input type="text" name="country">
-        <input type="submit" >
+        <label for="">Username:</label><br>
+        <input type="text" name="username"> <br>
+        <label for="">Password:</label><br>
+        <input type="password" name="password"> <br>
+        <input type="submit" name="login" value="Log in">
     </form>
 </body>
 </html>
 
 <?php
 
-    $capitals = array("USA"=>"Washington D.C.", 
-                    "Japan"=>"Tokyo", 
-                    "South Korea"=>"Seoul", 
-                    "Philippines"=>"Manila", 
-                    "France"=>"Paris");
+    foreach($_POST as $key => $value){
+        echo "{$key} = {$value} <br>";
+    }
+
+    if(isset($_POST["login"])){
+
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+        if(empty($username)){
+            echo "Please enter your username";
+        }
+        elseif(empty($password)){
+            echo "Please enter your password";
+        }
+        else{
+            echo "Hello {$username}!";
+        }
+
+    }
 
 
-    $capital = $capitals[$_POST["country"]];
-
-    echo $capital;
 ?>
