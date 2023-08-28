b3032f7f24af48505544df1dd9cce779fe831ae4
Dominique R. Libardo <72533293+d-libards@users.noreply.github.com>
2023-08-28 15:34:54 +0800

    sanitize/validate

--- PHP/index.php
+++ PHP/index.php
@@ -1,10 +1,46 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        username <br>
+        <input type="text" name="username"> <br>
+        age <br>
+        <input type="text" name="age"> <br>
+        email <br>
+        <input type="text" name="email"> <br>
+        <input type="submit" name="login" value="login">
+    </form>
+</body>
+</html>
 
-    function hypotenuse( $a, $b){
-        $c = sqrt($a ** 2 + $b ** 2);
-        return $c;
+
+<?php
+    if (isset($_POST['login'])){
+
+        
+        $age = filter_input(INPUT_POST, 'age', FILTER_VALIDATE_INT);
+        $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
+
+        if(empty($email)){
+            echo "That email is not valid.";
+        }
+        else{
+            echo "The {$email} is valid email";
+        }
+
+        //$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+        //$age = filter_input(INPUT_POST, "age", FILTER_SANITIZE_NUMBER_INT);
+        //$email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+
+        //echo "Hello, {$username} <br>";
+        //echo "You're {$age} years old";
+        //echo "Your email is: {$email}";
     }
 
-    echo hypotenuse(4, 4);
 
 ?>
