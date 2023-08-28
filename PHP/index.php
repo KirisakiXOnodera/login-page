05447af85d45c0c947ddaf777e6fc6a4809286e3
Dominique R. Libardo <72533293+d-libards@users.noreply.github.com>
2023-08-28 23:30:21 +0800

    connect

--- PHP/index.php
+++ PHP/index.php
@@ -1,12 +1,15 @@
 <?php
-    $password = "dom123";
-
-    $hash = password_hash($password, PASSWORD_DEFAULT);
-
-
-    if(password_verify("dom123", $hash)){
-        echo "login successful";
-    }else{
-        echo "login failed";
-    }
+    include("db.php");
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Hello</h2>
+</body>
+</html>
