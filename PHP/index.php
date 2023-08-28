657606b327702a47f1f37fb4e4232c128f6be4cc
Dominique R. Libardo <72533293+d-libards@users.noreply.github.com>
2023-08-28 18:28:28 +0800

    hash

--- PHP/index.php
+++ PHP/index.php
@@ -1,26 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="index.php" method="post">
-        <label for="">Username: </label> <br>
-        <input type="text" name="username">
-        <input type="submit">
-    </form>
-</body>
-</html>
-
 <?php
-    /*
-    foreach($_SERVER as $key => $value){
-        echo "{key} => {$value} <br>";
-    }
-    */
-    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-        echo "Hello World";
+    $password = "dom123";
+
+    $hash = password_hash($password, PASSWORD_DEFAULT);
+
+
+    if(password_verify("dom123", $hash)){
+        echo "login successful";
+    }else{
+        echo "login failed";
     }
 ?>
