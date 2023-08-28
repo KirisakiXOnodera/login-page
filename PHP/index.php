ad2a5ca7ee09fae27157b05130f7eb5a20b191ec
Dominique R. Libardo <72533293+d-libards@users.noreply.github.com>
2023-08-29 00:07:15 +0800

    insert

--- PHP/index.php
+++ PHP/index.php
@@ -1,15 +1,21 @@
 <?php
     include("db.php");
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h2>Hello</h2>
-</body>
-</html>
+    $username = "Patricks";
+    $password = "clarinet123";
+
+    $hash = password_hash($password, PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (user, password) VALUES ('$username', '$hash')";
+
+    try {
+        mysqli_query($conn, $sql);
+        echo "Success";
+    }
+    catch(mysqli_sql_exception){
+        echo "Error: no connection";
+    }
+
+
+    mysqli_close($conn);
+?>
